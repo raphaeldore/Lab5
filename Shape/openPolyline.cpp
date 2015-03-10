61c11f500b9bcf04de38448ef24972a6bdd13f68
Raphaël Doré <rdore@neomailbox.ch>
2015-03-10 15:07:40 -0400

    Ajout methodes add() et draw()  de openPolyLine + passation test relié

--- Shape/openPolyline.cpp
+++ Shape/openPolyline.cpp
@@ -2,3 +2,26 @@
 
 using namespace ShapeLibrary;
 
+OpenPolyline::~OpenPolyline()
+{
+	//delete windowApi;
+}
+
+void OpenPolyline::add(Point _point)
+{
+	points.push_back(_point);
+}
+
+void OpenPolyline::draw(const Color& _color)
+{
+	windowApi->setDrawingColor(_color);
+
+	for (auto it = points.begin(); it < points.end(); ++it)
+	{
+		if (next(it) != points.end())
+		{
+			windowApi->drawLine(*it, *next(it));
+		}
+		
+	}
+}
--- Shape/openPolyline.h
+++ Shape/openPolyline.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "stdafx.h"
+#include <memory>
 
 namespace ShapeLibrary
 {
 	class OpenPolyline
 	{
+	public:
+		OpenPolyline(IWindowAPI& _windowApi) : windowApi(&_windowApi) {};
+		~OpenPolyline();
 
+		void add(Point _point);
+		void draw(const Color& _color = Color::BLACK);
 
-
+	private:
+		IWindowAPI * windowApi;
+		vector<Point> points;
 	};
 }
