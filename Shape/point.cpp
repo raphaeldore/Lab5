875314aa6c6d1d65a3a32831271d67062caee8f5
Raphaël Doré <rdore@neomailbox.ch>
2015-03-10 14:07:29 -0400

    Impossible d'instancier un point avec une valeur de X ou Y negative. Passation test 1 & 2.

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -9,7 +9,7 @@ Point::Point( double _x, double _y ) :
 	x(_x), 
 	y(_y)
 {	
-	
+	if (_x < 0 || _y < 0) throw invalid_argument("X and Y must be positive");
 }
 
 bool Point::operator == (const Point & point) const
